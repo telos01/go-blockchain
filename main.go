ddbecde0d4b1e0dff8dd07c64c790d027a3f48ff
Telos <Cyber21fine@protonmail.com>
2022-01-30 20:08:56 +0300

    Proof of Work

--- main.go
+++ main.go
@@ -1,53 +1,23 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+	"strconv"
+
+	"github.com/telos01/go-chain/blockchain"
 )
 
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-type BlockChain struct {
-	blocks []*Block //array of pointers
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, preveHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), preveHash}
-	block.DeriveHash()
-	return block
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.PrevHash)
-	chain.blocks = append(chain.blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
-
 func main() {
-	chain := InitBlockChain()
-
-	for _, block := range chain.blocks {
+	chain := blockchain.InitBlockChain()
+	chain.AddBlock("Second block")
+	chain.AddBlock("Third block")
+	for _, block := range chain.Blocks {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW:%s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
